3f9718b02dcb7283f41d9bf4507e85d420c21aca
clg55 <claude.gomez55@gmail.com>
2020-09-23 15:32:37 +0100

    remove 5.0 from browsable_source

--- browsable_source/5.0/Linux-Windows/scilab-5.0/scilab/modules/overloading/macros/%b_i_st.sci
+++
@@ -1,3 +0,0 @@
-function out=%b_i_st(i,in,out)
-  out=generic_i_st(i,in,out)
-endfunction
--- browsable_source/5.0/Linux-Windows/scilab-5.0/scilab/modules/scicos/src/c/noscicos/noscicos.h
+++
@@ -1,8 +0,0 @@
-/*--------------------------------------------------------------------------*/
-/* INRIA 2007 */
-/* Sylvestre LEDRU */
-/*--------------------------------------------------------------------------*/ 
-
-int gw_scicos(void);
-int haltscicos(char *name, integer *win, integer *entry,long int name_len);
-
