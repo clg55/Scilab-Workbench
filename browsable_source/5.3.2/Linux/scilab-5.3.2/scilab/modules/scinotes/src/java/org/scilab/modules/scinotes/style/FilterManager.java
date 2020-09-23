1f5d46e8dc41802e751a824fe9617709751c3cd0
clg55 <claude.gomez55@gmail.com>
2020-09-23 15:17:29 +0100

    remove 5.3.2 from browsable_source

--- browsable_source/5.3.2/Linux/scilab-5.3.2/scilab/modules/scinotes/src/java/org/scilab/modules/scinotes/style/FilterManager.java
+++
@@ -1,9 +0,0 @@
-package org.scilab.modules.scinotes.style;
-
-import org.scilab.modules.scinotes.ScilabDocument;
-
-public class FilterManager {
-		
-	public FilterManager(final ScilabDocument scilabDocument) {
-	}
-}
